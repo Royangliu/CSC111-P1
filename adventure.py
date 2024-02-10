9fd710b2439b1f51f08a5d8273033952fef59267
Royangliu <149643400+Royangliu@users.noreply.github.com>
2024-02-10 01:19:34 +0000

    hm

--- adventure.py
+++ adventure.py
@@ -103,6 +103,20 @@ if __name__ == "__main__":
 
     previous_x = p.x
     previous_y = p.y
+    print("""
+    You've got an important exam coming up this evening, and you've been studying for weeks.
+    Last night was a particularly late night on campus.
+    You had difficulty focusing, so rather than staying in one place, you studied in various places throughout campus as the night progressed.
+    Unfortunately, when you woke up this morning, you were missing some 
+    important exam-related items. You cannot find your T-card, and you're nervous they 
+    won't let you into tonight's exam without it. Also, you seem to have misplaced your 
+    lucky exam pen -- even if they let you in, you can't possibly write with another pen! 
+    Finally, your instructor for the course lets you bring a cheat sheet - a handwritten page 
+    of information in the exam. Last night, you painstakingly crammed as much material onto 
+    a single page as humanly possible, but that's missing, too! 
+    All of this stuff must be around campus somewhere! 
+    Can you find all of it before your exam starts tonight?
+    """)
 
     while not p.victory and p.steps > 0:
         location = w.get_location(p.x, p.y)
